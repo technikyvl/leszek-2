b4b7cb5ed63743343c9e1967d4a628dda24e30ad
technikyvl <jakub4.biznes@gmail.com>
2025-10-19 19:55:06 +0200

    Update promo.tsx

--- components/promo.tsx
+++ components/promo.tsx
@@ -29,8 +29,8 @@ export default function Section() {
       </h3>
 
       <p className="absolute bottom-12 right-6 text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-5xl z-10">
-        Profesjonalne zdjęcia do dokumentów i fotografię biznesową.
-        Nawet paszportowe może wyglądać dobrze.
+        Profesjonalne zdjęcia do dokumentów i portrety biznesowe.
+        Szybko, tanio, obok urzędu.
       </p>
     </div>
   )
