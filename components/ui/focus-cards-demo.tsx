63a8a0e1642b112e6e344c9f0a6061ef737585c4
technikyvl <jakub4.biznes@gmail.com>
2025-10-20 00:45:29 +0200

    Update focus-cards-demo.tsx

--- components/ui/focus-cards-demo.tsx
+++ components/ui/focus-cards-demo.tsx
@@ -7,24 +7,8 @@ export function FocusCardsDemo() {
       src: "https://images.unsplash.com/photo-1554224155-6726b3ff858f?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Dowód Osobisty",
-      src: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Paszport",
-      src: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Fotografia Biznesowa",
+      title: "Sesje Studyjne",
       src: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Portrety Firmowe",
-      src: "https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Prawo Jazdy",
-      src: "https://images.unsplash.com/photo-1554224155-6726b3ff858f?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   ];
 
