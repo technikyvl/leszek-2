6788c233df540bf5c77a115695d0273b54945cd0
technikyvl <jakub4.biznes@gmail.com>
2025-10-19 14:01:25 +0200

    Update focus-cards-demo.tsx

--- components/ui/focus-cards-demo.tsx
+++ components/ui/focus-cards-demo.tsx
@@ -3,28 +3,28 @@ import { FocusCards } from "@/components/ui/focus-cards";
 export function FocusCardsDemo() {
   const cards = [
     {
-      title: "Sesja Ślubna w Plenerze",
-      src: "https://images.unsplash.com/photo-1519741497674-611481863552?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Fotografia Biznesowa",
+      src: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Ceremonia Ślubna",
-      src: "https://images.unsplash.com/photo-1606800052052-a08af7148866?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Fotografia Ślubna",
+      src: "https://images.unsplash.com/photo-1519741497674-611481863552?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Portret Narzeczonych",
-      src: "https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Zdjęcia do Dokumentów",
+      src: "https://images.unsplash.com/photo-1554224155-6726b3ff858f?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Zabawa Weselna",
-      src: "https://images.unsplash.com/photo-1519225421980-715cb0215aed?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Portrety Firmowe",
+      src: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Szczegóły Ślubne",
-      src: "https://images.unsplash.com/photo-1519741497674-611481863552?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Sesje Korporacyjne",
+      src: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Sesja Narzeczeńska",
-      src: "https://images.unsplash.com/photo-1606800052052-a08af7148866?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Fotografia Eventowa",
+      src: "https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   ];
 
