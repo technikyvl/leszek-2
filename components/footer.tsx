905bd337101cc77f7724b58e747c99f50f72cc26
technikyvl <jakub4.biznes@gmail.com>
2025-11-05 18:57:37 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,10 +8,10 @@ export default function Footer() {
     >
       <div className="relative h-[calc(100vh+400px)] sm:h-[calc(100vh+600px)] lg:h-[calc(100vh+800px)] -top-[100vh]">
         <div className="h-[400px] sm:h-[600px] lg:h-[800px] sticky top-[calc(100vh-400px)] sm:top-[calc(100vh-600px)] lg:top-[calc(100vh-800px)]">
-          <div className="bg-neutral-900 py-4 sm:py-6 lg:py-8 px-4 sm:px-6 h-full w-full flex flex-col justify-between">
+          <div className="bg-neutral-900 pt-4 sm:pt-6 lg:pt-8 pb-8 sm:pb-12 lg:pb-16 px-4 sm:px-6 h-full w-full flex flex-col justify-between">
             <div className="flex shrink-0 gap-8 sm:gap-12 lg:gap-20" />
             <div className="flex flex-col sm:flex-row justify-between items-start sm:items-end gap-4 sm:gap-0">
-              <h1 className="text-[18vw] sm:text-[16vw] lg:text-[14vw] leading-[0.8] mt-4 sm:mt-6 lg:mt-10 text-white font-bold tracking-tight">
+              <h1 className="text-[18vw] sm:text-[16vw] lg:text-[14vw] leading-[0.85] mt-4 sm:mt-6 lg:mt-10 mb-4 sm:mb-0 text-white font-bold tracking-tight">
                 FOTO EXPRESS LESZEK JAKIEŁA
               </h1>
               <div className="flex flex-col items-start sm:items-end gap-3">
